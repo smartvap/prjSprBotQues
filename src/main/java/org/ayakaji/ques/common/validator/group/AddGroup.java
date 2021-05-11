6fb474e6051e10072c83acc44090e3e9326e4703
wanglei <2239264903@qq.com>
2021-05-11 10:02:54 +0800

    author: wanglei title: 项目信息查询 date: 2021-05-11

--- src/main/java/org/ayakaji/ques/common/validator/group/AddGroup.java
+++
@@ -1,6 +0,0 @@
-package org.ayakaji.ques.common.validator.group;
-/**
- * 新增数据
- */
-public interface AddGroup {
-}
--- src/main/java/org/ayakaji/ques/common/validator/group/UpdateGroup.java
+++
@@ -1,7 +0,0 @@
-package org.ayakaji.ques.common.validator.group;
-
-/**
- * 更新数据
- */
-public interface UpdateGroup {
-}
